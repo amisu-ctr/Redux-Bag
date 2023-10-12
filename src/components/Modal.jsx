d4a177ba315d6b7c73e195ad9d0c3a845c96ab94
amisu-ctr <hamisurahman@gmail.com>
2023-10-12 16:06:01 +0000

    Calculate items feature and Modal Comonent.

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+function Modal() {
+  return (
+    <aside className="modal-container">
+      <div className="modal">
+        <h4>remove all items from our shopping cart?</h4>
+        <div className="btn-container">
+          <button type="button" className="btn confirm-btn">
+            confirm
+          </button>
+          <button className="btn clear-btn">cancel</button>
+        </div>
+      </div>
+    </aside>
+  );
+}
+export default Modal;
